d4fce1932effd0bd828f054cb0426a91490081e3
stefangreve <greve.stefan@outlook.jp>
2021-01-30 22:46:58 +0100

    Implement loadMenu and loadPage functions as well as some utility methods

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const PAGES = ['home', 'exhibitions', 'contact'];
+
+var capitalize = (string) => string.replace(/^\w/, (c) => c.toUpperCase());
+
+function documentReady(callback) {
+    if (document.readyState != 'loading') {
+        callback();
+    } else {
+        document.addEventListener('DOMContentLoaded', callback);
+    }
+}
+
+function loadMenu(file) {
+    if (file == 'index.html') {
+        window.location.replace('http://127.0.0.1:5500/index.html');
+    } else {
+        loadPage('main', file);
+    }
+}
+
+function loadPage(container, file) {
+    fetch(file)
+    .then(function(response) {
+        return response.text();
+    })
+    .then(function(body) {
+        document.querySelector(container).innerHTML = body;
+    });
+    let id = file.split('/')[1].replace('.html', '');
+    document.title = capitalize(id);
+    
+    PAGES.forEach(function(page) {
+        document.getElementById(page).classList.remove('active');
+    });
+    
+    document.getElementById(id).classList.add('active');
+}
+
+var copyright = document.getElementById("copyright"); 
+copyright.innerHTML = `Anneliese Greve &copy; ${new Date().getFullYear()}`;
