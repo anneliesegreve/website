c1d88ca21529bbda5df1d65bfd28ac50f08a74e3
stefangreve <greve.stefan@outlook.jp>
2021-02-16 16:09:54 +0100

    Remove admin page from const list and update title format

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const PAGES = ['works', 'info', 'legal_notice', 'admin'];
+const PAGES = ['works', 'info', 'legal_notice'];
 
 var capitalize = (string) => string.replace(/^\w/, (c) => c.toUpperCase());
 
@@ -27,7 +27,7 @@ function inject(container, file) {
 function loadPage(container, file) {
     inject(container, file);
     let id = file.split('/')[1].replace('.html', '');
-    document.title = `Anne &${id == 'legal_notice' ? 'Legal Notice' : capitalize(id)}`;
+    document.title = `Anneliese Greve | ${id == 'legal_notice' ? 'Legal Notice' : capitalize(id)}`;
 
     PAGES.forEach(function (page) {
         document.getElementById(page).classList.remove('active');
