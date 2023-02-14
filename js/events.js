773c2692f8913a64a1113b4db182e08c1478bf46
StefanGreve <greve.stefan@outlook.jp>
2023-02-14 22:24:18 +0100

    Fix formatting

--- js/events.js
+++ js/events.js
@@ -1,11 +1,12 @@
-const scrollToTop =  () => window.scrollTo({ top: 0, behavior: 'smooth' });
+const scrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
 function copyMailAddress(element) {
-    navigator.clipboard.writeText(element.innerHTML.trim())
+  navigator.clipboard
+    .writeText(element.innerHTML.trim())
     .then(() => {
-        alert('Copied mail address to clipboard!');
+      alert("Copied mail address to clipboard!");
     })
-    .catch(err => {
-        console.error(`Could not copy text to clipboard: ${err}`);
+    .catch((err) => {
+      console.error(`Could not copy text to clipboard: ${err}`);
     });
 }
